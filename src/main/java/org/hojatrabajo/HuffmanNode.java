18576bb3bf8c87d0c73e837476ce18d9901452d0
carlosaltan18 <carlosaltan850@gmail.com>
2026-03-18 11:25:44 -0600

    HuffmanNode

--- src/main/java/org/hojatrabajo/HuffmanNode.java
+++ src/main/java/org/hojatrabajo/HuffmanNode.java
@@ -1,5 +1,31 @@
 package org.hojatrabajo;
 
-public class HuffmanNode {
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    char c;
+    HuffmanNode left;
+    HuffmanNode right;
+    int frecuencia;
 
+    public HuffmanNode(char c, HuffmanNode left, HuffmanNode right) {
+        this.c = c;
+        this.left = null;
+        this.right = null;
+        this.frecuencia = frecuencia;
+    }
+
+    public HuffmanNode(int frecuencia, HuffmanNode izq, HuffmanNode der) {
+        this.c = '\0';
+        this.frecuencia = frecuencia;
+        this.left = izq;
+        this.right = der;
+    }
+
+    public boolean esHoja() {
+        return left == null && right == null;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode otro) {
+        return this.frecuencia - otro.frecuencia;
+    }
 }
